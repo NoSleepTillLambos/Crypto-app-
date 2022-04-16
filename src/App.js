88bef07392e684461a37acd9463ea36ecb27a764
Liam Wedge <21100218@virtualwindow.co.za>
2022-04-17 00:40:12 +0200

    Final commit
    Added search tab and additional styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import './App.css';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import Axios from "axios";
-
+import Coin from './components/Coin';
 
 function App() {
 
-    useEffect(() =>{
-      Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=5&currency=EUR")
-      .then((res) => {
-        console.log(res.data)
-      })
-    },[])
+  const [listOfCoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState("")
+
+
+  useEffect(() =>{
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&currency=EUR")
+    .then((res) => {
+      setListOfCoins(res.data.coins)
+    })
+  },[])
+  
+  const filteredCoins = listOfCoins.filter((coin)=> {
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  })
 
   return (
 
     <div className="App">
-      <div className="crypto-header"></div>
-      <div className="crypto-display"></div>
+      <div className="crypto-header">
+        <input type="text" placeholder="BTC..." onChange={(event) => {setSearchWord(event.target.value)}}></input>
+      </div>
+
+      <div className="crypto-display">{filteredCoins.map((coin)=> {
+
+        return <Coin 
+        name={coin.name} 
+        icon={coin.icon} 
+        price={coin.price} 
+        symbol={coin.symbol}/>
+
+      })}</div>
     </div>
   );
 }
